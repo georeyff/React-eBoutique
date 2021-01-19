98b2c27f0278851c09157d0f2fdb96e286a58393
Geoffrey <geoff2912@hotmail.fr>
2021-01-19 17:33:12 +0100

    Product Screen

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -5,10 +5,11 @@ const data = {
             name:'Nike Slim Shirt',
             category:'Shirts',
             image:'/images/p1.jpg',
-            price:120,
+            price: 120,
+            countInStock: 10,
             brand:'Nike',
-            rating:4.5,
-            numReviews:10,
+            rating: 4.5,
+            numReviews: 10,
             description:'high quality product'
         },
         {
@@ -16,10 +17,11 @@ const data = {
             name:'Addidas Fit Shirt',
             category:'Shirts',
             image:'/images/p2.jpg',
-            price:100,
+            price: 100,
+            countInStock: 20,
             brand:'Addidas',
-            rating:4,
-            numReviews:12,
+            rating: 4,
+            numReviews: 12,
             description:'high quality product'
         },
         {
@@ -27,10 +29,11 @@ const data = {
             name:'Lacoste Slim Shirt',
             category:'Shirts',
             image:'/images/p3.jpg',
-            price:110,
+            price: 110,
+            countInStock: 0,
             brand:'Lacoste',
-            rating:4.8,
-            numReviews:8,
+            rating: 4.8,
+            numReviews: 8,
             description:'high quality product'
         },
         {
@@ -38,10 +41,11 @@ const data = {
             name:'Nike Slim Pant',
             category:'Pants',
             image:'/images/p4.jpg',
-            price:78,
+            price: 78,
+            countInStock: 15,
             brand:'Nike',
-            rating:4.5,
-            numReviews:14,
+            rating: 4.5,
+            numReviews: 14,
             description:'high quality product'
         },
         {
@@ -49,10 +53,11 @@ const data = {
             name:'Puma Slim Pant',
             category:'Pants',
             image:'/images/p5.jpg',
-            price:65,
+            price: 65,
+            countInStock: 5,
             brand:'Puma',
-            rating:4.5,
-            numReviews:10,
+            rating: 4.5,
+            numReviews: 10,
             description:'high quality product'
         },
         {
@@ -60,10 +65,11 @@ const data = {
             name:'Lacoste Slim Pant',
             category:'Pants',
             image:'/images/p6.jpg',
-            price:139,
+            price: 139,
+            countInStock: 12,
             brand:'Lacoste',
-            rating:4.7,
-            numReviews:20,
+            rating: 4.7,
+            numReviews: 20,
             description:'high quality product'
         },
     ]
