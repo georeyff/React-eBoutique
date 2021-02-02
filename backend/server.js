a72dc79f7b37cd1e0b55966b4ed878e3be356488
Geoffrey <geoff2912@hotmail.fr>
2021-02-02 14:48:26 +0100

    Create Place Order API

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 // import data from './data.js';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
@@ -32,6 +33,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/eboutique', {
 // });
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
